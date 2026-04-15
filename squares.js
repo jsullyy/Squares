1e1ce665d3c90aca0b09d45690ef314330af2bc8
Joe Sullivan <jasoccer28@outlook.com>
2026-04-15 10:41:54 -0400

    updated all Squares

--- squares.js
+++ squares.js
@@ -1,14 +1,15 @@
-let squareArea = document.querySelector("squarearea");
+let squareArea = document.querySelector("#squarearea");
 // let squareArea = document.getElementById("squarearea")
 let squareCount = parseInt(Math.random()*21)+30; // 30-50
-
+//Make some variable for a really big zIndex
 for(let i = 0;i<squareCount;i++){
     //make a square
     let square = document.createElement("div");
     square.className = "square";
     square.style.left = parseInt(Math.random()*650)+"px";
     square.style.top = parseInt(Math.random()*250)+"px";
-    square.style.backgroundColor = "#0F2345";
+    square.style.backgroundColor = getRandomColor();
+    // I can put a onclick and a function(this)
     //put it on the screen
     squareArea.append(square);
 }
@@ -20,5 +21,17 @@ function getRandomColor(){
     for(let i = 0;i <6;i++){
         color+=letters.charAt(parseInt (Math.random()*letters.length));
     }
+    //return that string
     return color;
+}
+
+//make a function to add one square
+//Make sure that I set that as the onclick for the button
+
+//Make a function that changes all of the colors of the squares
+//Make sure the button calls it
+
+function changeColors(){
+    let allSquares = document.querySelectorAll(".square");
+
 }
